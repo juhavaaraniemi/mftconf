63a4190a1e3d1e97f51af2314fa5ed9b93123d8a
juhavaaraniemi <100115205+juhavaaraniemi@users.noreply.github.com>
2023-01-27 14:01:07 +0200

    Create mftconf_example.lua

---
+++ mftconf_example.lua
@@ -0,0 +1,27 @@
+-- mftconf example
+
+package.loaded["mftconf/lib/mftconf"] = nil
+mftconf = require('mftconf/lib/mftconf')
+PATH = _path.data.."mft_config/"
+
+function init()
+  mft = midi.connect(2)
+end
+
+function key(n,z)
+  if z == 1 and n == 2 then
+    mftconf.load_conf(mft,PATH.."mft_molly.mfs")
+  end
+end
+
+function redraw()
+  screen.clear()
+  screen.level(15)
+  screen.move(0,11)
+  screen.text("give midi device &")
+  screen.move(0,18)
+  screen.text("filename as params")
+  screen.move(0,32)
+  screen.text("press k2 to load config")
+  screen.update()
+end
